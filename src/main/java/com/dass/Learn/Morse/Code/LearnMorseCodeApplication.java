6a94697f670dde2ce7c3dabc6574deff530b5fad
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2020-01-31 15:38:04 -0800

    setup

---
+++ src/main/java/com/dass/Learn/Morse/Code/LearnMorseCodeApplication.java
@@ -0,0 +1,13 @@
+package com.dass.Learn.Morse.Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMorseCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMorseCodeApplication.class, args);
+	}
+
+}
