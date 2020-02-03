b7b82ca610f2e91bc51d7a85f795317e7e513015
Shane Gable <gableshane@gmail.com>
2020-02-03 13:19:00 -0800

    templates/static/etc

--- src/main/java/com/dass/LearnMorseCode/LearnMorseCodeApplication.java
+++ src/main/java/com/dass/LearnMorseCode/LearnMorseCodeApplication.java
@@ -1,4 +1,4 @@
-package com.dass.Learn.Morse.Code;
+package com.dass.LearnMorseCode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
