e0a20fb23194cd7a45f573303d04bfd6a12dfb66
Dayne Daylong <dalexad@gmail.com>
2020-02-05 15:01:53 -0800

    added inbox button to profile and all users diplay on friends

--- src/main/java/com/dass/LearnMorseCode/controllers/MessageController.java
+++ src/main/java/com/dass/LearnMorseCode/controllers/MessageController.java
@@ -1,9 +1,11 @@
 package com.dass.LearnMorseCode.controllers;
 
+
 import com.dass.LearnMorseCode.models.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class MessageController {
