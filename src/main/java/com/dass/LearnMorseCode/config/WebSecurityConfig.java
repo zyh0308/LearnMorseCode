38b3e72122a7aef9cfe5e4625bb76e391e4228a6
Dayne <57049229+Gr8-Dayne@users.noreply.github.com>
2020-02-05 11:33:04 -0800

    Update WebSecurityConfig.java

--- src/main/java/com/dass/LearnMorseCode/config/WebSecurityConfig.java
+++ src/main/java/com/dass/LearnMorseCode/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .cors().disable()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/", "/login", "/create", "/*.css","/*.jpg","/*.png","/audiotest","/*.js","/*.wav","/englishToMorse","/morseToEnglish","/message").permitAll()
+            .antMatchers("/", "/login", "/create", "/*.css","/*.jpg","/*.png","/audiotest","/*.js","/*.wav","/englishToMorse","/morseToEnglish","/message","/about").permitAll()
             .anyRequest().authenticated()
             .and()
                 .formLogin()
